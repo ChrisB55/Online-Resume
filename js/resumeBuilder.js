222367f61f4e457d3c9e055293f5fc0a9463459d
Chris Brown <chrisbrown@Robin-Colaninnos-iMac.local>
2016-08-01 10:34:39 -0400

    Add early changes to online resume

---
+++ js/resumeBuilder.js
@@ -0,0 +1,8 @@
+var name = "Christopher W Brown"
+var formattedName = HTMLheaderName.replace("%data%", name);
+
+var role = "Web Developer";
+var formattedRole = HTMLheaderRole.replace("%data%", role)
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
