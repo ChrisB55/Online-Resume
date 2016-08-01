e91a954822cdfe833adaf8a3a64ad5672f578d91
Christopher W. Brown <christopherwilliambrown@gmail.com>
2016-08-01 13:36:35 -0400

    Add files via upload
    Add objects for work, education, projects, bio

---
+++ resumeBuilder.js
@@ -0,0 +1,70 @@
+var bio = {
+  "name" : "Christopher Brown",
+  "role" : "Web Developer",
+  "contacts" : {
+    "mobile": "202 555-5555",
+    "email" : "christopherXXXX@gmail.com",
+    "github" : "https://github.com/Chrisx55",
+    "twitter" : "@chrisX",
+    "location" : "Washington D.C.",
+  },
+  "welcomeMessage": "Welcome to my Resume page.",
+  "skills": [
+    "Web development", "Digital Communications", "Brazilian Jiu Jitsu"
+  ],
+  "bioPic": "images/fry.jpg"
+}
+
+var education = {
+  "schools": [
+    {
+      "name": "Guilford College",
+      "city": "Greensboro",
+      "degree": "BA",
+      "Major" : "Political Science",
+      "url" : "http://www.guilfordcollege.edu"
+    },
+    {
+      "schools": [
+        {
+          "name": "Catholic University of America",
+          "city": "Washington, D.C.",
+          "degree": "MA",
+          "Major" : "Political Science",
+          "url" : "http://www.cua.edu"
+    }
+  ],
+  "onlineCourses": [
+    "title" : "Programming Foundations: HTML5 + CSS3 for Entrepreneurs",
+    "school" : "Udemy",
+    "dates" : "2016",
+    "url" : "https://www.udemy.com/html-css-more/learn/v4/overview"
+  ]
+}
+
+var work = {
+  "jobs": [
+    {
+      "employer": "Media Matters",
+      "title" : "Deputy Team Leader",
+      "dates" : "2011-2013",
+      "description" : "Lorem ipsum dolor sit amet, consectetur adipiscing."
+    }
+  ]
+}
+
+var project = {
+  "projects":[
+    {
+    "title" : "Sample project",
+    "date" : "2016",
+    "description" : "Lorem ipsum dolor sit amet, consectetur adipiscing."
+    }
+  ]
+}
+
+
+
+
+// $("#main").append(work["position"]);
+// $("#main").append(education.name);
